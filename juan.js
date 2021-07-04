18f33ddc26c20e395cd1ea27b4568bbcb5907ac8
Jesica <guerrero191094@gmail.com>
2021-07-04 18:21:59 -0300

    modifica juan

--- juan.js
+++ juan.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
+
+
 let miNombre ="juan cabrera";
-let edad= 15
-=======
-var peliculasPiolas = ["Titanic", "300", "Troya", "Tu vieja en tanga"]
-let miNombre = "Ernesto Sanchez capo"
->>>>>>> a7db7c13f354cd525b3d8b64b661cf9f731cd921
+
